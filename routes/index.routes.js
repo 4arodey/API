9440e048dd774a3acfd0e56375f7e4a0760d95b9
o.karpach <9038ffff>
2019-02-14 13:16:36 +0300

    SCDF-1248 Create the ping route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,6 +3,7 @@ const usersRoutes = require('./user.routes');
 
 function sendRoutes(app, router) {
   usersRoutes(app, router);
+  app.use("/ping", (req, res) => {res.json({})});
 }
 
 module.exports = sendRoutes;
