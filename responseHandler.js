ef1ff73e9aff759d19478957b522dfd16b491008
o.karpach <9038ffff>
2019-02-21 14:25:22 +0300

    SCDF-1248 Install swagger in the server and generate swagger routes

--- responseHandler.js
+++ responseHandler.js
@@ -1,6 +1,7 @@
 const HTTP_CODES = require('./httpCodes');
 const logger = require('./src/logger');
-require('dotenv/config');
+
+const appConfig = require('./app.config');
 
 function handleSuccess(actionFn) {
   return (req, res, next) => {
@@ -24,23 +25,34 @@ function handleError(app) {
 
     const errObj = {
       messsage: '',
-      stackTrase: '',
-      type: 'Validation Error',
+      stackTrace: '',
+      type: '',
     };
 
-    if (process.env.NODE_ENV === 'developer') {
-      errObj.messsage = err.message;
-      errObj.stackTrase = err.stack;
 
-      if (err.status === 500) {
-        errObj.messsage = 'Internal server error';
-      }
-    }
+    errObj.messsage = appConfig.NODE_ENV === 'developer'
+      ? err.message
+      : 'Internal server error';
 
-    if (err.status === 500) {
+    errObj.stackTrace = appConfig.NODE_ENV === 'developer'
+      ? err.stack
+      : '';
+
+    errObj.type = err.status === HTTP_CODES.CLIENT_ERROR
+      ? 'Validation Error'
+      : 'Internal Server Error';
+
+
+    if (err.status === HTTP_CODES.SERVER_ERROR) {
       logger.error(err);
     }
-    res.status(HTTP_CODES.SERVER_ERROR);
+
+    const httpErrorCode = err.status === HTTP_CODES.CLIENT_ERROR
+      ? HTTP_CODES.CLIENT_ERROR
+      : HTTP_CODES.SERVER_ERROR;
+
+    res.status(httpErrorCode);
+
     res.send(errObj);
   });
 }
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,6 @@ const responseHandler = require('../responseHandler');
 function sendRoutes(app, router) {
   usersRoutes(app, router);
   app.get('/ping', responseHandler.handleSuccess(() => ({})));
-  app.get('/error', () => {throw error})
 }
 
 module.exports = sendRoutes;
